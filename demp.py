32a217480ff7607655204abdea8bcc3aca67cdd1
zhangyong <zahngyong@exiao.tech>
2023-03-27 19:23:12 +0800

    1

--- demp.py
+++ demp.py
@@ -1,7 +1,7 @@
 import json
 import base64
-from Crypto.Cipher import PKCS1_v1_5 as Cipher_pksc1_v1_5
-from Crypto.PublicKey import RSA
+from crypto.Cipher import PKCS1_v1_5 as Cipher_pksc1_v1_5
+from crypto.PublicKey import RSA
 
 
 def _encrpt(string, public_key):
